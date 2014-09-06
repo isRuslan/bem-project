55f9bfc553e9b5c79bd9f6dbf318be731bb81d63
Ruslan Ismagilov <is.ismagilov@gmail.com>
2014-09-06 18:43:34 +0400

    add browser support

--- index.js
+++ index.js
@@ -2,38 +2,42 @@
 /**
  * Welcome to the Popo!
  */
-(function (exports) {
+(function () {
 
-/**
- * @constructor popo
- *
- * @param {Object} opt Initialization object
- * @return {Function} other `Default` function
- */
-function popo (opt, other) {
-	var keys = [];
+	/**
+	 * @constructor popo
+	 *
+	 * @param {Object} opt Initialization object
+	 * @return {Function} other `Default` function
+	 */
+	function popo (opt, other) {
+		var keys = [];
 
-	// check
-	for (var i in opt) {
-		if (opt.hasOwnProperty(i)) {
-			if ( typeof opt[i] != "function" ) {
-				opt[i] = undefined;
-			} else {
-				keys.push(i);
+		// check
+		for (var i in opt) {
+			if (opt.hasOwnProperty(i)) {
+				if ( typeof opt[i] != "function" ) {
+					opt[i] = undefined;
+				} else {
+					keys.push(i);
+				}
 			}
 		}
+
+		return function() {
+	    return !!~keys.indexOf(arguments.length.toString())
+	    	? opt[arguments.length].apply(this, arguments)
+	    	: other.apply(this, arguments);
+	  }
 	}
 
-	return function() {
-    return !!~keys.indexOf(arguments.length.toString())
-    	? opt[arguments.length].apply(this, arguments)
-    	: other.apply(this, arguments);
-  }
-}
+	/**
+	 * Module exports
+	 */
+	if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+	  module.exports = popo;
+	} else {
+	  window.popo = popo;
+	}
 
-/**
- * Module exports
- */
-module.exports = popo;
-
-})(typeof module.exports === 'undefined' ? this['popo'] = {} : module.exports);
+})();
